f48237ca23d7d9002d4fd62dfa3c6bfcc997615f
mashalla <GKC_Alex@web.de>
2011-05-27 11:06:58 +0200

    Dialog init

---
+++ Source/demo.js
@@ -0,0 +1,25 @@
+// -------------------------------------------------------------------
+//
+// last edit: 2011 - 02 - 18
+//
+// author: Christian Merz
+//
+// instantiation of different dialogs
+//
+// --------------------------------------------------------------------
+
+
+
+window.addEvent('domready', function(){
+
+    var modaldemo = new AscModalAlertConfirm();
+    
+    $('dialogDemo').addEvent('click', function(e){
+        e = new Event(e).stop();
+        modaldemo.show_demo('e', '', 'Demo Dialog', "<p>This is an Wai Aria Demo Dialog</p><p>Please enter information.</p><label for='field1'>field 1: </label><input id='field1' type='text'><br><br><label for='field2'>field 2: </label><input id='field2'type='text'>", "Submit", 'i16 ok16', function(e){
+            this.hide();
+        });
+    }
+.bind(this));
+    
+});
