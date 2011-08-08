1d5d814842c0c79411700f0d836974d7a87069c3
mashalla <GKC_Alex@web.de>
2011-08-08 14:40:57 +0200

    new version

--- Source/demo.js
+++ Source/demo.js
@@ -1,30 +1,29 @@
-/*
----
-description: accessible dialog based on ascribe dialog
- 
-license: MIT-style
- 
-authors:
-- Christian Merz
- 
-requires:
-- core/1.3: '*'
-- more/1.2.4: Fx.Elements
- 
-provides: Accessible Dialog
- 
-version: 1.0
-...
-*/
 window.addEvent('domready', function(){
 
-    var modaldemo = new AscModalAlertConfirm();
+    var dialogCme = new DialogCME({
+		'alert':false,
+		'closeButton':true,
+		'title':'DemoDialog',
+		'content':"<p>This is an Wai Aria Demo Dialog</p><p>Please enter information.</p><label for='field1'>field 1: </label><input id='field1' type='text'><br><br><label for='field2'>field 2: </label><input id='field2'type='text'>",
+		'submit':{
+			'exists':true,
+			'value':'Submit',
+			'fn': function(e){
+				this.hide()
+			}
+		},		
+		'cancel':{
+			'exists':true,
+			'value':'Cancel',
+			'fn': function(e){
+				this.hide()
+			}
+		}
+	});
     
     $('dialogDemo').addEvent('click', function(e){
         e = new Event(e).stop();
-        modaldemo.show_demo('e', '', 'Demo Dialog', "<p>This is an Wai Aria Demo Dialog</p><p>Please enter information.</p><label for='field1'>field 1: </label><input id='field1' type='text'><br><br><label for='field2'>field 2: </label><input id='field2'type='text'>", "Submit", 'i16 ok16', function(e){
-            this.hide();
-        });
+        dialogCme.show();
     }
 .bind(this));
     
